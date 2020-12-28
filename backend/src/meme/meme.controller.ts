7b44830ff00e2d12123ecddce7f82cca3d7425ce
mayerph <ph.mayer@campus.lmu.de>
2020-12-28 11:24:51 +0100

    added error handling to upload functions

--- backend/src/meme/meme.controller.ts
+++ backend/src/meme/meme.controller.ts
@@ -9,8 +9,11 @@ export class MemeController {
   constructor() {
     this.insertTestData()
   }
-
+  /**
+   * initiate test environment with some sample data
+   */
   insertTestData() {
+    Meme.deleteMany({}).exec()
     const memes_tmp = [
       {
         name: "drake",
@@ -31,31 +34,42 @@ export class MemeController {
     ]
     memes_tmp.forEach((e) => new Meme(e).save())
   }
-
+  /**
+   * returns all available meme template
+   */
   async memes(): Promise<IMeme[]> {
     const memes: IMeme[] = await Meme.find()
     return memes
   }
 
-  writeMemeTemplate(image: any) {
+  /**
+   * write image to file
+   * @param image object with the image (meta)data
+   */
+  writeMemeTemplate(image: any): Promise<IMeme> {
     return new Promise((resolve, reject) => {
-      if (image.data && image.name) {
-        fs.writeFile(
-          "./" + config.storage.templates.path + image.name,
-          image.data,
-          async (err) => {
-            if (err) {
-              reject()
-            }
-            const memeDoc: IMeme = {
-              name: image.name,
-              file: image.name
-            }
-            const meme = await new Meme(memeDoc).save()
-            resolve(meme)
-          }
-        )
+      if (!image.data || !image.name) {
+        reject(new Error("no image data"))
       }
+      fs.writeFile(
+        "./" + config.storage.templates.path + image.name,
+        image.data,
+        async (err) => {
+          if (err) {
+            reject(err)
+          }
+          const memeDoc: IMeme = {
+            name: image.name,
+            file: image.name
+          }
+          try {
+            const meme: IMeme = await new Meme(memeDoc).save()
+            resolve(meme)
+          } catch (err) {
+            reject(err)
+          }
+        }
+      )
     })
   }
 }
--- backend/src/meme/meme.routes.ts
+++ backend/src/meme/meme.routes.ts
@@ -4,20 +4,28 @@ import { MemeController } from "./meme.controller"
 
 const router = express.Router()
 const memeController = new MemeController()
-
+/**
+ * route to all available meme templates
+ */
 router.get("", async (req: Request, res: Response, next: NextFunction) => {
   const memes = await memeController.memes()
   res.json(memes)
 })
 
-// upload image
+/**
+ * route for uploading meme templates
+ */
 router.post(
   "/upload",
   async (req: Request, res: Response, next: NextFunction) => {
     if (!req.files) {
       res.status(500)
     }
-    const result = await memeController.writeMemeTemplate(req.files?.meme)
+    const result = await memeController
+      .writeMemeTemplate(req.files?.meme)
+      .catch((err) => {
+        res.status(500)
+      })
     res.send(result)
   }
 )
