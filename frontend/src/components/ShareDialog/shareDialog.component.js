24bd890ea27e3bc91820c01c62709264751783ff
mayerph <ph.mayer@campus.lmu.de>
2021-01-07 20:17:59 +0100

    refactored to functional component

--- frontend/src/components/ShareDialog/shareDialog.component.js
+++ frontend/src/components/ShareDialog/shareDialog.component.js
@@ -1,4 +1,10 @@
-import React, { forwardRef } from 'react'
+import React, {
+  forwardRef,
+  useEffect,
+  useRef,
+  useCallback,
+  useState,
+} from 'react'
 import { connect } from 'react-redux'
 
 import Avatar from '@material-ui/core/Avatar'
@@ -30,17 +36,18 @@ import * as config from '../../config.json'
 /**
  * Dialog component for initiating the share of a meme
  */
-class ShareDialog extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      copySnackbar: false,
+const ShareDialog = (props) => {
+  const [openedSnack, setOpenedSnack] = useState(false)
+  const socialLinksRef = useCallback((node) => {
+    if (node !== null) {
+      node.scrollTop = 0
     }
-  }
+  }, [])
+
   /**
    * general style settings
    */
-  styles = (theme) => ({
+  const styles = (theme) => ({
     root: {
       margin: 0,
       padding: theme.spacing(2),
@@ -56,7 +63,7 @@ class ShareDialog extends React.Component {
   /**
    * Dialog-title inclusive close button
    */
-  dialogTitle = withStyles(this.styles)((props) => {
+  const DialogTitle = withStyles(styles)((props) => {
     const { children, classes, onClose, ...other } = props
     return (
       <MuiDialogTitle disableTypography className={classes.root} {...other}>
@@ -75,7 +82,7 @@ class ShareDialog extends React.Component {
   /**
    * Dialog footer inclusive style
    */
-  dialogFooter = withStyles((theme) => ({
+  const DialogFooter = withStyles((theme) => ({
     root: {
       padding: theme.spacing(2),
     },
@@ -84,7 +91,7 @@ class ShareDialog extends React.Component {
   /**
    * wrapper element for setting a divider
    */
-  dialogDivider = withStyles((theme) => ({
+  const DialogDivider = withStyles((theme) => ({
     root: {
       paddingTop: theme.spacing(2),
       paddingBottom: theme.spacing(2),
@@ -93,14 +100,11 @@ class ShareDialog extends React.Component {
     },
   }))(MuiDialogContent)
 
-  componentDidMount() {
-    console.log('hellooooooo')
-  }
   /**
    * gets called so the list can gets scrolled through from the top
    * @param {*} ref dom reference of the social media list
    */
-  initializeScrollTop(ref) {
+  const initializeScrollTop = (ref) => {
     console.log('initializeScrollTop')
     if (ref && ref.scrollTop) {
       // ref.scrollTop = 0
@@ -110,7 +114,7 @@ class ShareDialog extends React.Component {
   /**
    * List consting of all offered social media platforms
    */
-  platforms = [
+  const platforms = [
     {
       name: 'WhatsApp',
       source: 'https://api.whatsapp.com/send?text=',
@@ -134,147 +138,127 @@ class ShareDialog extends React.Component {
     { name: 'E-Mail', source: 'mailto:?body=', icon: <EmailIcon /> },
   ]
 
-  openSnackbar() {
+  const openSnackbar = () => {
     console.log('openSnackbar')
-    this.setState({ ...this.state, copySnackbar: true })
+    setOpenedSnack(true)
   }
 
-  closeSnackbar() {
+  const closeSnackbar = () => {
     console.log('closeSnackbar')
-    this.setState({ ...this.state, copySnackbar: false })
+    setOpenedSnack(false)
   }
 
-  setRef() {}
+  const proxySetting = `${config.frontend.proxy.protocol}://${config.frontend.proxy.server}:${config.frontend.proxy.port}/`
+  const frontendSetting = `${config.frontend.protocol}://${config.frontend.server}:${config.frontend.port}/`
 
-  componentDidMount() {
-    console.log('componentDidMount')
-  }
-
-  /**
-   * renders the component
-   */
-  render() {
-    const DialogTitle = this.dialogTitle
-    const DialogDivider = this.dialogDivider
-    const DialogFooter = this.dialogFooter
-
-    const proxySetting = `${config.frontend.proxy.protocol}://${config.frontend.proxy.server}:${config.frontend.proxy.port}/`
-    const frontendSetting = `${config.frontend.protocol}://${config.frontend.server}:${config.frontend.port}/`
-
-    const destination = config.frontend.proxy.enabled
-      ? proxySetting
-      : frontendSetting
-    const meme = this.props.meme
-      ? this.props.meme
-      : {
-          name: '06220ac0-4e95-11eb-b7d3-334230f5957c.png',
-          route: '/images/memes/06220ac0-4e95-11eb-b7d3-334230f5957c.png',
-          template: {
-            id: '5ff1df51a28fb193a50f1c60',
-            name: 'Drake-Hotline-Bling.jpg',
-            route: '/templates/Drake-Hotline-Bling.jpg',
-          },
-          captions: [
-            {
-              text: 'hello world',
-              position: {
-                x: 0,
-                y: 0,
-              },
-              color: 'green',
-              size: 60,
+  const destination = config.frontend.proxy.enabled
+    ? proxySetting
+    : frontendSetting
+  const meme = props.meme
+    ? props.meme
+    : {
+        name: '06220ac0-4e95-11eb-b7d3-334230f5957c.png',
+        route: '/images/memes/06220ac0-4e95-11eb-b7d3-334230f5957c.png',
+        template: {
+          id: '5ff1df51a28fb193a50f1c60',
+          name: 'Drake-Hotline-Bling.jpg',
+          route: '/templates/Drake-Hotline-Bling.jpg',
+        },
+        captions: [
+          {
+            text: 'hello world',
+            position: {
+              x: 0,
+              y: 0,
             },
-          ],
-        }
+            color: 'green',
+            size: 60,
+          },
+        ],
+      }
 
-    const { open, onClose } = this.props
+  const { open, onClose } = props
 
-    const handleClose = () => {
-      onClose()
-    }
-
-    return (
-      <>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          open={this.testen}
-          autoHideDuration={3000}
-          onClose={() => this.closeSnackbar()}
-          message="Note archived"
-          action={
-            <React.Fragment>
-              <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={() => this.closeSnackbar()}
-              >
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </React.Fragment>
-          }
-        />
-        <Dialog
-          className="social-dialog"
-          onClose={handleClose}
-          aria-labelledby="simple-dialog-title"
-          open={open}
-          fullWidth={true}
-          maxWidth="xs"
-        >
-          <DialogTitle id="custom-dialog-title" onClose={handleClose}>
-            Share
-          </DialogTitle>
-          <DialogDivider className="dialog-content" dividers>
-            <div
-              className="social-list-wrapper"
-              ref={(ref) => this.initializeScrollTop(ref)}
-            >
-              <List className="social-list">
-                {this.platforms.map((platform) => (
-                  <a
-                    className="social-link"
-                    href={platform.source + destination + 'meme/' + meme.id}
-                    target="_blank"
-                    rel="noreferrer"
-                    key={'link' + platform.name}
-                  >
-                    <ListItem key={platform.name}>
-                      <ListItemAvatar>
-                        <Avatar>{platform.icon}</Avatar>
-                      </ListItemAvatar>
-                      <ListItemText primary={platform.name} />
-                    </ListItem>
-                  </a>
-                ))}
-                <ListItem autoFocus button onClick={() => this.openSnackbar()}>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <LinkIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary="Link kopieren" />
-                </ListItem>
-              </List>
-            </div>
-          </DialogDivider>
-          <DialogFooter className="dialog-footer">
-            Choose a share option. The link will open the platform in a new tab.
-          </DialogFooter>
-        </Dialog>
-      </>
-    )
+  const handleClose = () => {
+    onClose()
   }
-}
 
-ShareDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
+  useEffect(() => {
+    // 3) access a ref, for example 0
+    //console.log('useEffect', observed())
+  })
+
+  return (
+    <>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={openedSnack}
+        autoHideDuration={3000}
+        onClose={() => closeSnackbar()}
+        message="Note archived"
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={() => closeSnackbar()}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+      <Dialog
+        className="social-dialog"
+        onClose={handleClose}
+        aria-labelledby="simple-dialog-title"
+        open={open}
+        fullWidth={true}
+        maxWidth="xs"
+      >
+        <DialogTitle id="custom-dialog-title" onClose={handleClose}>
+          Share
+        </DialogTitle>
+        <DialogDivider className="dialog-content" dividers>
+          <div className="social-list-wrapper" ref={socialLinksRef}>
+            <List className="social-list">
+              {platforms.map((platform) => (
+                <a
+                  className="social-link"
+                  href={platform.source + destination + 'meme/' + meme.id}
+                  target="_blank"
+                  rel="noreferrer"
+                  key={'link' + platform.name}
+                >
+                  <ListItem key={platform.name}>
+                    <ListItemAvatar>
+                      <Avatar>{platform.icon}</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={platform.name} />
+                  </ListItem>
+                </a>
+              ))}
+              <ListItem autoFocus button onClick={() => openSnackbar()}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <LinkIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Link kopieren" />
+              </ListItem>
+            </List>
+          </div>
+        </DialogDivider>
+        <DialogFooter className="dialog-footer">
+          Choose a share option. The link will open the platform in a new tab.
+        </DialogFooter>
+      </Dialog>
+    </>
+  )
 }
-ShareDialog.defaultProps = {}
 
 export default connect()(ShareDialog)
