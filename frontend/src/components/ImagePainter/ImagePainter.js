c2cb9a0cc2219ba25ed52a8eab0b33865fa13e95
nam.kang@campus.lmu.de <nam.kang@campus.lmu.de>
2021-02-18 18:40:02 +0100

    slight styling in draw

--- frontend/src/components/ImagePainter/ImagePainter.js
+++ frontend/src/components/ImagePainter/ImagePainter.js
@@ -14,35 +14,37 @@ class ImagePainter extends React.Component {
 
   render() {
     return (
-      <ReactPainter
-        width={500}
-        height={500}
-        onSave={(blob) => {
-          console.log(blob)
-          var touse = blob
-          var date = new Date().valueOf()
-          var newfile = new File([touse], 'drawing' + date + '.png', {
-            type: touse.type,
-          })
-          var fd = new FormData()
-          fd.append('template', newfile)
-          console.log(fd)
-          axios
-            .post('http://localhost:2000/templates/', fd, {})
-            .then((res) => {
-              console.log(res.statusText)
+      <div className="memedraw">
+        <ReactPainter
+          width={500}
+          height={500}
+          onSave={(blob) => {
+            console.log(blob)
+            var touse = blob
+            var date = new Date().valueOf()
+            var newfile = new File([touse], 'drawing' + date + '.png', {
+              type: touse.type,
             })
-            .then((result) => {
-              this.onApiLoad()
-            })
-        }}
-        render={({ triggerSave, canvas }) => (
-          <div>
-            <div>{canvas}</div>
-            <button onClick={triggerSave}>Save as Template</button>
-          </div>
-        )}
-      />
+            var fd = new FormData()
+            fd.append('template', newfile)
+            console.log(fd)
+            axios
+              .post('http://localhost:2000/templates/', fd, {})
+              .then((res) => {
+                console.log(res.statusText)
+              })
+              .then((result) => {
+                this.onApiLoad()
+              })
+          }}
+          render={({ triggerSave, canvas }) => (
+            <div>
+              <div>{canvas}</div>
+              <button onClick={triggerSave}>Save as Template</button>
+            </div>
+          )}
+        />
+      </div>
     )
   }
 }
