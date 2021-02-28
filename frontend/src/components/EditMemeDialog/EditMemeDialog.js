a3e11d7df4d8e55a6c77dc3e819fee1e71433b62
Balthasar Schüss <mail@bschuess.dev>
2021-02-28 13:35:01 +0100

    Add add image to meme template redirect

--- frontend/src/components/EditMemeDialog/EditMemeDialog.js
+++ frontend/src/components/EditMemeDialog/EditMemeDialog.js
@@ -13,6 +13,7 @@ import {
   DialogTitle,
   Divider,
 } from '@material-ui/core'
+import { Redirect } from 'react-router-dom'
 
 import { withStyles } from '@material-ui/core/styles'
 import CloseIcon from '@material-ui/icons/Close'
@@ -24,45 +25,81 @@ import MuiDialogTitle from '@material-ui/core/DialogTitle'
 class EditMemeDialog extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { redirect: false }
+  }
+
+  redirect(url, wipe) {
+    const element = {
+      x: 0,
+      y: 0,
+      width: 400,
+      bounds: '#meme-canvas',
+      type: 'image',
+      imageUrl: url,
+    }
+    if (wipe) {
+      this.props.dispatch({type: 'WIPE_ELEMENTS'})
+    }
+
+    this.props.dispatch({ type: 'ADD_ELEMENT', element: element })
+    this.setState({ redirect: true })
   }
 
   render() {
     const meme = this.props.meme_data
     return (
-      <Dialog
-        className="download-dialog"
-        onClose={() => this.props.onClose()}
-        aria-labelledby="simple-dialog-title"
-        open={this.props.open}
-        fullWidth={true}
-        maxWidth="xs"
-      >
-        <DialogTitle>Add to/create meme editor</DialogTitle>
-        <Divider />
-        <List>
-          <ListItem autoFocus button onClick={() => alert('replacing')}>
-            <ListItemAvatar>
-              <Avatar>
-                <BuildIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Start from scratch" />
-          </ListItem>
-          <ListItem autoFocus button onClick={() => alert('adding')}>
-            <ListItemAvatar>
-              <Avatar>
-                <AddBoxIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Add image" />
-          </ListItem>
-        </List>
-        <Divider />
-        <MuiDialogContent>
-          Choose whether to add the image to the current editor or to replace
-          the current editor content.
-        </MuiDialogContent>
-      </Dialog>
+      <>
+        {this.state.redirect ? (
+          <Redirect
+            to={{
+              pathname: '/imagememe',
+            }}
+          />
+        ) : (
+          <Dialog
+            className="download-dialog"
+            onClose={() => this.props.onClose()}
+            aria-labelledby="simple-dialog-title"
+            open={this.props.open}
+            fullWidth={true}
+            maxWidth="xs"
+          >
+            <DialogTitle>Add to/create meme editor</DialogTitle>
+            <Divider />
+            <List>
+              <ListItem
+                autoFocus
+                button
+                onClick={() => this.redirect(meme.url, true)}
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <BuildIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Start from scratch" />
+              </ListItem>
+              <ListItem
+                autoFocus
+                button
+                onClick={() => this.redirect(meme.url, false)}
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <AddBoxIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Add image" />
+              </ListItem>
+            </List>
+            <Divider />
+            <MuiDialogContent>
+              Choose whether to add the image to the current editor or to
+              replace the current editor content.
+            </MuiDialogContent>
+          </Dialog>
+        )}
+      </>
     )
   }
 }
